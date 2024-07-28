f0923d95c183c662b016d0707792e2474b1ff38f
김희영 <168628325+heeyoungis@users.noreply.github.com>
2024-07-28 16:07:40 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -1,15 +1,14 @@
 while True:
-    t = list(map(int, input(). split()))
+    t = list(map(int, input().split()))
     if t[0] == t[1] == t[2] == 0:
         break
-    elif t[0] == t[1] == t[2]:
-        print('Equilateral')
-    else:
-        t.sort()
-        if t[2] >= t[0] + t[1]:
+    t.sort()
+    if t[2] >= t[0] + t[1]:
             print('Invalid')
+    else:
+        if t[0] == t[1] == t[2]:
+            print('Equilateral')
+        elif t[0] == t[1] or t[1] == t[2]:
+            print('Isosceles')
         else:
-            if t[0] == t[1] or t[1] == t[2]:
-                print('Isosceles')
-            else:
-                print('Scalene')
+            print('Scalene')
